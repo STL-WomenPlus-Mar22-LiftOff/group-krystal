14d4a0a552e78466f2650240aee80ed72ae92a94
EmmaHalsey <ekhalsey@yahoo.com>
2022-12-24 11:50:30 -0600

    added bootstrap to angular project and created a component that contains symptom management form

--- liftOff/src/app/app.module.ts
+++ liftOff/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SymptomManagementFormComponent } from './symptom-management-form/symptom-management-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SymptomManagementFormComponent
   ],
   imports: [
     BrowserModule
