aa061993eff272d4b180a4945a63d9c8593ec628
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2022-12-28 13:29:39 -0500

    date done

--- liftOff/src/app/daily-tracker-form/daily-tracker-form.component.ts
+++ liftOff/src/app/daily-tracker-form/daily-tracker-form.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-daily-tracker-form',
   templateUrl: './daily-tracker-form.component.html',
   styleUrls: ['./daily-tracker-form.component.css']
+  
 })
 export class DailyTrackerFormComponent implements OnInit {
 
-  date = Date.prototype.getDate();
+  currentDate = formatDate(new Date(), 'EEEE, MMMM d, y', 'en');
 
   constructor() { }
 
