d50a4adc4447178fa8d40f28c58c77e279e4b57b
MJNambiar <marinanambiar@nikhils-air.lan>
2022-12-30 17:28:59 -0600

    back end api endpoint added for user

--- tracker-app-backend/src/main/java/org/launchcode/trackerappbackend/TrackerAppBackendApplication.java
+++ tracker-app-backend/src/main/java/org/launchcode/trackerappbackend/TrackerAppBackendApplication.java
@@ -1,5 +1,8 @@
 package org.launchcode.trackerappbackend;
 
+import org.launchcode.trackerappbackend.data.UserRepository;
+import org.launchcode.trackerappbackend.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- tracker-app-backend/src/main/java/org/launchcode/trackerappbackend/controllers/UserController.java
+++ tracker-app-backend/src/main/java/org/launchcode/trackerappbackend/controllers/UserController.java
@@ -1,5 +1,28 @@
 package org.launchcode.trackerappbackend.controllers;
 
+import org.launchcode.trackerappbackend.data.UserRepository;
+import org.launchcode.trackerappbackend.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("")
+    public Iterable<User> getUser(){
+
+        //uncomment this section for the first bootrun for hardcoded data to be added in MySQL, re-comment for any additional bootruns- otherwise will get error since you are adding the exact same data twice
+//            userRepository.save(new User("Emma", "emma@gmail.com", "emma123"));
+//            userRepository.save(new User("Emily", "emily@gmail.com", "emily123"));
+
+        return userRepository.findAll();
+    }
 }
