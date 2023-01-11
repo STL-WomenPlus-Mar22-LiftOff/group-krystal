b112d96442c1741b2d271d4e07b3fd63a82cf682
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2023-01-11 13:07:15 -0600

    symptom controller post mapping added

--- tracker-app-backend/src/main/java/org/launchcode/trackerappbackend/controllers/SymptomController.java
+++ tracker-app-backend/src/main/java/org/launchcode/trackerappbackend/controllers/SymptomController.java
@@ -2,11 +2,12 @@ package org.launchcode.trackerappbackend.controllers;
 
 import org.launchcode.trackerappbackend.data.SymptomRepository;
 import org.launchcode.trackerappbackend.models.Symptom;
+import org.launchcode.trackerappbackend.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -21,4 +22,11 @@ public class SymptomController {
         return symptomRepository.findAll();
     }
 
+    @PostMapping("")
+    void addSymptom(@RequestBody @Valid Symptom symptom, Errors errors) {
+        if (!errors.hasErrors()) {
+            Symptom newSymptom = new Symptom(symptom.getSymptomName());
+            symptomRepository.save(newSymptom);
+        }
+    }
 }
