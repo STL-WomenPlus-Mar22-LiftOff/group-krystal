971a62735170f597f0a0a18b071d33c89be789ff
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2023-01-15 10:09:07 -0600

    symptom controller updated

--- Trackfully-Server/src/main/java/org/launchcode/TrackfullyServer/controllers/SymptomController.java
+++ Trackfully-Server/src/main/java/org/launchcode/TrackfullyServer/controllers/SymptomController.java
@@ -3,10 +3,10 @@ package org.launchcode.TrackfullyServer.controllers;
 import org.launchcode.TrackfullyServer.data.SymptomRepository;
 import org.launchcode.TrackfullyServer.models.Symptom;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -19,6 +19,14 @@ public class SymptomController {
     @GetMapping("")
     public Iterable<Symptom> getSymptom(){
         return symptomRepository.findAll();
+    }
+
+    @PostMapping("")
+    void addSymptom(@RequestBody @Valid Symptom symptom, Errors errors) {
+        if (!errors.hasErrors()) {
+            Symptom newSymptom = new Symptom(symptom.getSymptomName());
+            symptomRepository.save(newSymptom);
+        }
     }
 
 }
