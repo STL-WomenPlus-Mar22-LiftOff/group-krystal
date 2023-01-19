6336a63293af2d5aaa62d672aab3bde6157adbf7
EmmaHalsey <ekhalsey@yahoo.com>
2023-01-18 20:57:58 -0600

    working on backend

--- Trackfully-Client/src/app/model/daily-entry.ts
+++ Trackfully-Client/src/app/model/daily-entry.ts
@@ -1,7 +1,7 @@
 import { SubjectSubscriber } from "rxjs/internal/Subject";
 
 export class DailyEntry {
-    id: number = 0;
+   
     dateToday: string = "";
     symptLevel: number = 0;
 //add which symptom is associated with this entry?
