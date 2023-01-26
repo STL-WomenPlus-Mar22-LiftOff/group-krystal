96edb66a702a4bb5bb2836fa0547f8b83f8d795d
jennyryoo <jennyjryoo@gmail.com>
2023-01-26 09:07:45 -0800

    updated chart visuals with title, grid lines, setting axis, and tracker controller to filter dates

--- Trackfully-Client/src/app/charts/charts.component.ts
+++ Trackfully-Client/src/app/charts/charts.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Chart, registerables} from 'chart.js';
 import {ChartService} from './chart.service'
+import { SymptomService } from '../service/symptom/symptom.service';
+import { Symptom } from '../model/symptom';
+import 'chartjs-adapter-date-fns';
 
 @Component({
   selector: 'app-charts',
@@ -10,18 +13,28 @@ import {ChartService} from './chart.service'
 
 export class ChartsComponent implements OnInit {
 
-  constructor(private  chartService: ChartService) {
-    Chart.register(...registerables);
+  symptomInfo: Symptom;
+  symptomLabel: String = "data";
+  userName: any;
+
+constructor(private  chartService: ChartService, private symptomService: SymptomService) {
+  Chart.register(...registerables);
+  this.symptomInfo = new Symptom;
   }  
 
-  ngOnInit(): void {
-    let symptomId = sessionStorage.getItem("symptomId");
-    if (symptomId) {
-      this.chartService.getData(symptomId).subscribe((data: any) => {
-      console.log(data);
-      this.createChart(data[0],data[1]);
-    })
-  }
+ngOnInit(): void {
+  let symptomId = sessionStorage.getItem("symptomId");
+  if (symptomId) {
+    this.symptomService.getSymptomById(parseInt(symptomId)).subscribe((result) => {
+      this.symptomInfo = result;
+      this.symptomLabel = this.symptomInfo.symptomName;
+  });
+  this.chartService.getData(symptomId).subscribe((data: any) => {
+    // console.log(data);
+    this.createChart(data[0],data[1]);
+  })
+};
+  this.userName = sessionStorage.getItem("name");
 }
 
   public chart: any;
@@ -30,17 +43,43 @@ export class ChartsComponent implements OnInit {
   
     this.chart = new Chart("MyChart", {
       type: 'line',
+
       data: {
         labels: dates,
         datasets: [{
-          label: 'Testing Data Points',
+          label: this.symptomLabel.toString(),
           data: ratings,
-          borderColor: "#3e95cd",
-          fill: true
+          borderColor: "#519188",
+          fill: false,
+          pointBackgroundColor: "#344B47",
         }]
       },
       options: {
         responsive: true,
+        plugins: {
+          title: {
+            display: true,
+            text: `${this.userName}'s Symptoms for Last 30 Days`
+          }
+        },
+        scales: {
+          y: {
+            min: 0,
+            max: 10,
+            grid: {
+              display: false
+            }
+          },
+          x: {
+            grid: {
+              display: false
+            },
+            type: 'time',
+            time: {
+              unit: 'day',
+            }
+          }
+        }
     }
   }
   );
