63ddf9cdba57e5a94b3a82e76bcd18b3ea96a4eb
Marina Nambiar <107495361+MJNambiar@users.noreply.github.com>
2023-01-26 14:53:03 -0600

    included name, rerouting, and 2 versions of nav bar

--- Trackfully-Client/src/app/navigation-bar/navigation-bar.component.ts
+++ Trackfully-Client/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AutheticationService } from '../service/authentication/authetication.service';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -7,15 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationBarComponent implements OnInit {
 
-  menuItems = [
+  //need to create logged in menuItems: already exists but need to add Welcome <user> in html and access user name from session storage here
+  //and logged out menuItems: need log in and register buttons
+  loggedInMenuItems = [
     {linkId: 1, linkName: 'Dashboard', linkUrl: 'dashboard'},
     {linkId: 2, linkName: 'Daily Tracker', linkUrl: 'daily-tracker-form'}, 
     {linkId: 3, linkName: 'Sign Out', linkUrl: 'logout'}
   ];
 
-  constructor() { }
+  loggedOutMenuItems = [
+    {linkId: 1, linkName: 'Log In', linkUrl: ''},
+    {linkId: 2, linkName: 'Register', linkUrl: 'sign-up'}, 
+  ];
 
-  ngOnInit(): void {
+    userName : String = ""; 
+    navBarType : String = "loggedIn";
+
+  constructor(private authenticationService: AutheticationService) { 
+    
   }
 
+//only occurring when refreshing the page? not with re-routing
+  ngOnInit(): void {
+      if (this.authenticationService.isUserLoggedIn()) {
+       this.userName = sessionStorage.getItem("name") || "";
+       console.log("user name" + this.userName);
+       this.navBarType = "loggedIn";
+      } else {
+        this.navBarType = "loggedOut";
+      }
+    }
 }
+
+
+//see name in nav bar when logged in
+//see log out vs log in when logged in/vs out
+//route user to dashboard if try to go to localhost//4200
