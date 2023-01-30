e442914e20bc75eea6b16c924137b2e71fc4dc19
unknown <slashley127@gmail.com>
2023-01-29 18:47:45 -0600

    ngx-slider installed, new slider added to html

--- Trackfully-Client/src/app/daily-tracker-form/daily-tracker-form.component.ts
+++ Trackfully-Client/src/app/daily-tracker-form/daily-tracker-form.component.ts
@@ -10,6 +10,7 @@ import { Observable } from 'rxjs';
 import { identifierModuleUrl } from '@angular/compiler';
 import { User } from '../model/user';
 import { UserService } from '../service/user/user.service';
+import { Options } from '@angular-slider/ngx-slider';
 
 @Component({
   selector: 'app-daily-tracker-form',
@@ -17,6 +18,23 @@ import { UserService } from '../service/user/user.service';
   styleUrls: ['./daily-tracker-form.component.css'],
 })
 export class DailyTrackerFormComponent implements OnInit {
+
+  value: number = 5;
+  options: Options = {
+    showTicksValues: true,
+    stepsArray: [
+      {value: 0},
+      {value: 2},
+      {value: 3},
+      {value: 4},
+      {value: 5},
+      {value: 6},
+      {value: 7},
+      {value: 8},
+      {value: 9},
+      {value: 10}
+    ]
+  };
 
   currentDate = formatDate(new Date(), 'EEEE, MMMM d, y', 'en');
   dailyEntry: DailyEntry;
