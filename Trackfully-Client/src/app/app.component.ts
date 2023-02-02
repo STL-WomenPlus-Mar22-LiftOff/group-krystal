dfbe1ce0627f7c8f8f1838da6e236407db431810
Marina Nambiar <107495361+MJNambiar@users.noreply.github.com>
2023-02-02 14:27:57 -0600

    made changes to app files for sticky footer

--- Trackfully-Client/src/app/app.component.ts
+++ Trackfully-Client/src/app/app.component.ts
@@ -3,12 +3,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: `
+  // <app-navigation-bar></app-navigation-bar>
+  
+  // <router-outlet></router-outlet>
+  
+  // <app-footer></app-footer>
+  // `,
   template: `
+  
   <app-navigation-bar></app-navigation-bar>
   
-  <router-outlet></router-outlet>
-  
-  <app-footer></app-footer>
+  <div id="page-container">
+    <div id="content-wrap">
+      <router-outlet></router-outlet>
+    </div>
+    <app-footer></app-footer>
+  </div>
   `,
   styles: [],
   //templateUrl: './app.component.html',
